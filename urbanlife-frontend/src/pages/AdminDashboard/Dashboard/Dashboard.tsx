bedb00c46d20bfbd88fcc65de76d43826e6b3e09
RanuPutra <raya.ranuputra@gmail.com>
2025-06-14 23:54:35 +0700

    refactor: update admin dashboard styles import paths

--- urbanlife-frontend/src/pages/AdminDashboard/Dashboard/Dashboard.tsx
+++ urbanlife-frontend/src/pages/AdminDashboard/Dashboard/Dashboard.tsx
@@ -1,7 +1,7 @@
 import Chart from "react-apexcharts";
-import "../../../styles/Dashboard/Dashboard.css";
+import "../../../styles/AdminDashboard/Dashboard/Dashboard.css";
 import Calendar from "react-calendar";
-import "../../../styles/Dashboard/CalendarCustom.css";
+import "../../../styles/AdminDashboard/Dashboard/CalendarCustom.css";
 import Orders from "../Order/Order";
 
 function Dashboard() {

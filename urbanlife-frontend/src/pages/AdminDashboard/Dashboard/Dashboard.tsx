e46da22dba0786ac54ded52ae9f8f755f8c484c1
ccheryll1 <rylche663@gmail.com>
2025-06-15 21:29:04 +0700

    apus map

--- urbanlife-frontend/src/pages/AdminDashboard/Dashboard/Dashboard.tsx
+++ urbanlife-frontend/src/pages/AdminDashboard/Dashboard/Dashboard.tsx
@@ -3,7 +3,6 @@ import "../../../styles/AdminDashboard/Dashboard/CalendarCustom.css";
 import ChartBar from "../../../components/AdminDashboard/charts/bar/ChartBar";
 import CardWidget from "../../../components/AdminDashboard/card/CardWidget";
 import RecentOrders from "../../../components/AdminDashboard/orders/RecentOrders";
-import CardWorld from "../../../components/AdminDashboard/map/CardWorld";
 
 function Dashboard() {
 
@@ -14,18 +13,16 @@ function Dashboard() {
         <div className="col-span-6 ">
           <ChartBar />
         </div>
-
         <div className="col-span-4">
           <CardWidget />
         </div>  
       </div>
+
       <div className="grid grid-cols-10 gap-5 mt-5">
         <div className="col-span-6 ">
           <RecentOrders />
         </div>
-
         <div className="col-span-4">
-          <CardWorld />
         </div>  
 
       </div>

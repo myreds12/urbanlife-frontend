379f09e8e699c1a33f2b6268e7309c04b23e8583
ccheryll1 <rylche663@gmail.com>
2025-06-15 23:40:09 +0700

    dashboard : +cardwelcome

--- urbanlife-frontend/src/pages/AdminDashboard/Dashboard/Dashboard.tsx
+++ urbanlife-frontend/src/pages/AdminDashboard/Dashboard/Dashboard.tsx
@@ -1,16 +1,20 @@
 import "../../../styles/AdminDashboard/Dashboard/Dashboard.css";
 import "../../../styles/AdminDashboard/Dashboard/CalendarCustom.css";
-import ChartBar from "../../../components/AdminDashboard/charts/bar/ChartBar";
-import CardWidget from "../../../components/AdminDashboard/card/CardWidget";
-import RecentOrders from "../../../components/AdminDashboard/orders/RecentOrders";
-import CardWorld from "../../../components/AdminDashboard/map/CardWorld";
+import ChartBar from "../../../components/AdminDashboard/Dashboard/charts/bar/ChartBar";
+import CardWidget from "../../../components/AdminDashboard/Dashboard/card/CardWidget";
+import RecentOrders from "../../../components/AdminDashboard/Dashboard/orders/RecentOrders";
+import CardWelcome from "../../../components/AdminDashboard/Dashboard/card/CardWelcome";
 
 function Dashboard() {
 
 
   return (
     <div className="p-2">
-      <div className="grid grid-cols-10 gap-5">
+      <div className="grid grid-cols-1 gap-5">
+        <CardWelcome />
+      </div>
+
+      <div className="grid grid-cols-10 gap-5 mt-5">
         <div className="col-span-6 ">
           <ChartBar />
         </div>

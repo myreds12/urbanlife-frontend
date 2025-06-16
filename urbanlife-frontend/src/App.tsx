701a30fc51aa416cd778c95f56ca319d5b81aa68
ccheryll1 <rylche663@gmail.com>
2025-06-16 16:10:17 +0700

    apusapus

--- urbanlife-frontend/src/App.tsx
+++ urbanlife-frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import AppLayout from "./layouts/AdminDashboard/AppLayout.tsx";
 import Dashboard from "./pages/AdminDashboard/Dashboard/Dashboard";
 import Order from "./pages/AdminDashboard/Order/Order";
 import DayTour from "./pages/AdminDashboard/DayTour/DayTour";
@@ -9,14 +8,12 @@ import WhatsappConnect from "./pages/AdminDashboard/WhatsappSetting/WhatsappConn
 function App() {
   return (
     <Router>
-      <AppLayout>
         <Routes>
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/order" element={<Order />} />
           <Route path="/day-tour" element={<DayTour />} />
           <Route path="/whatsapp-connect" element={<WhatsappConnect />} />
         </Routes>
-      </AppLayout>
     </Router>
   );
 }

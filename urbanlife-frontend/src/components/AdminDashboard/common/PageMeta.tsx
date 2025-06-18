95510a1b38d4eae7d1c53774aba996b05913f86b
RanuPutra <raya.ranuputra@gmail.com>
2025-06-19 03:05:52 +0700

    ??>?>?>.._

--- urbanlife-frontend/src/components/AdminDashboard/common/PageMeta.tsx
+++ urbanlife-frontend/src/components/AdminDashboard/common/PageMeta.tsx
@@ -1,4 +1,4 @@
-import { HelmetProvider, Helmet } from "react-helmet-async";
+import { useEffect } from "react";
 
 const PageMeta = ({
   title,
@@ -6,15 +6,22 @@ const PageMeta = ({
 }: {
   title: string;
   description: string;
-}) => (
-  <Helmet>
-    <title>{title}</title>
-    <meta name="description" content={description} />
-  </Helmet>
-);
+}) => {
+  useEffect(() => {
+    document.title = title;
 
-export const AppWrapper = ({ children }: { children: React.ReactNode }) => (
-  <HelmetProvider>{children}</HelmetProvider>
-);
+    const metaDesc = document.querySelector('meta[name="description"]');
+    if (metaDesc) {
+      metaDesc.setAttribute("content", description);
+    } else {
+      const meta = document.createElement("meta");
+      meta.name = "description";
+      meta.content = description;
+      document.head.appendChild(meta);
+    }
+  }, [title, description]);
+
+  return null;
+};
 
 export default PageMeta;

9fa0f92dbba5be38f781ffa2709fd027d9545d3b
RanuPutra <raya.ranuputra@gmail.com>
2025-06-23 23:16:27 +0700

    feat(landing-page): add hero section component with card form

--- urbanlife-frontend/src/components/LandingPage/HeroSection.jsx
+++ urbanlife-frontend/src/components/LandingPage/HeroSection.jsx
@@ -1,21 +1,21 @@
 import "../../styles/LandingPage/HeroSection.css";
-import backgroundImage from '../../../public/images/landingpage.jpg';
 import CardForm from "./CardForm";
 
 const HeroSection = () => {
   return (
-    <>
-    <div className="hero-section justify-between gap-40" style={{ backgroundImage: `url(${backgroundImage})` }}> 
-      <div style={{ maxWidth: "650px", margin: "" }}>
+    <div
+      className="hero-section justify-between gap-40"
+      style={{ backgroundImage: `url('/images/landingpage.jpg')` }} // langsung dari public
+    >
+      <div style={{ maxWidth: "650px" }}>
         <p className="text-7xl/23 font-bold text-left text-white">
           Discover the beauty places around the world
         </p>
       </div>
-      <div className="">
+      <div>
         <CardForm />
       </div>
     </div>
-    </>
   );
 };
 

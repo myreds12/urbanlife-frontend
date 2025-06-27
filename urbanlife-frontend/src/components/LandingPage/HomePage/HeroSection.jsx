fb2aed146884f024d929bdcdce641b5460a611eb
RanuPutra <raya.ranuputra@gmail.com>
2025-06-27 15:55:17 +0700

    style(HeroSection): adjust gap spacing for better visual

--- urbanlife-frontend/src/components/LandingPage/HomePage/HeroSection.jsx
+++ urbanlife-frontend/src/components/LandingPage/HomePage/HeroSection.jsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
       className="hero-section"
       style={{ backgroundImage: `url('/images/landingpage.jpg')` }}
     >
-      <div className="max-w-7xl mx-auto px-4 flex flex-col md:flex-row justify-between gap-6 md:gap-10 py-10 md:py-20">
+      <div className="max-w-7xl mx-auto px-4 flex flex-col md:flex-row justify-between gap-6 md:gap-60 py-10 md:py-20">
         <div className="hidden md:block" style={{ maxWidth: "650px" }}>
           <p className="playfair text-7xl/23 font-bold text-left text-white">
             Discover the beauty places around the world

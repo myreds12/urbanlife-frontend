c06cf169499231d9ca8f8ac960b5e0283f3c71a0
ccheryll1 <rylche663@gmail.com>
2025-07-18 23:57:05 +0700

    style(HeroSection) : improve layout spacing and cardform sizing

--- urbanlife-frontend/src/components/LandingPage/HomePage/HeroSection.jsx
+++ urbanlife-frontend/src/components/LandingPage/HomePage/HeroSection.jsx
@@ -7,16 +7,22 @@ const HeroSection = () => {
       className="hero-section"
       style={{ backgroundImage: `url('/images/LandingPage/HeroSection/landingpage2.jpg')` }}
     >
-      <div className="max-w-7xl mx-auto mt-10 px-6 flex flex-col md:flex-row justify-between gap-6 md:gap-4 py-10 md:py-20">
-        <div className="hidden md:block" style={{ maxWidth: "900px" }}>
+      <div className="max-w-7xl mx-auto mt-10 px-6 flex flex-col md:flex-row justify-between items-start md:items-center gap-6 md:gap-16 lg:gap-50 py-10 md:py-20">
+
+        {/* Teks Desktop */}
+        <div className="hidden md:block" style={{ width: "60%" }}>
           <p className="playfair text-7xl/25 font-bold text-left text-white py-5 ml-5">
             Discover <br />
-            the 
-            beauty places 
+            the beauty places <br />
             around the world
           </p>
         </div>
-        <div className="w-full md:w-auto ">
+
+        {/* CardForm */}
+        <div
+          className="w-full md:w-auto"
+          style={{ minWidth: "350px", maxWidth: "400px", flexShrink: 0 }}
+        >
           <CardForm />
         </div>
       </div>

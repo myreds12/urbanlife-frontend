a248e73983531837155f77f88c44c1397087ac5a
ccheryll1 <rylche663@gmail.com>
2025-08-07 12:07:33 +0700

    fix(Footer) : update contact information

--- urbanlife-frontend/src/components/LandingPage/HomePage/Footer.jsx
+++ urbanlife-frontend/src/components/LandingPage/HomePage/Footer.jsx
@@ -224,7 +224,7 @@ const Footer = () => {
               <div className="space-y-2">
                 <p className="text-gray-300">Jakarta Selatan</p>
                 <p className="text-gray-300">Indonesia</p>
-                <p className="text-gray-300">+62 (0) 816 919 812</p>
+                <p className="text-gray-300">+62 816 919 812</p>
                 <p className="text-gray-300"> info@urbanlife.id </p>
               </div>
             </div>

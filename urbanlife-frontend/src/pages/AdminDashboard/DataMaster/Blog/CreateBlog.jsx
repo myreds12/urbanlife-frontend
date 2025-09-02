e9e80e5b46e05d1b32609867193535fe4af79597
ccheryll1 <rylche663@gmail.com>
2025-09-02 18:27:42 +0700

    fix(CreateBlog): update link path from '/admin/blog' to '/admin/blogs'

--- urbanlife-frontend/src/pages/AdminDashboard/DataMaster/Blog/CreateBlog.jsx
+++ urbanlife-frontend/src/pages/AdminDashboard/DataMaster/Blog/CreateBlog.jsx
@@ -269,7 +269,7 @@ const CreateBlog = () => {
             )}
 
             <div className="flex justify-end gap-3 px-6 pb-6 mt-5">
-              <Link to="/admin/blog">
+              <Link to="/admin/blogs">
                 <button type="button" className="px-4 py-2 bg-gray-300 text-gray-700 rounded-md hover:bg-gray-400">
                   Cancel
                 </button>

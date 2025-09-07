4a9f46cac3785ebc5c0fae2520266d18151254d1
ccheryll1 <rylche663@gmail.com>
2025-09-07 14:57:07 +0700

    feat: Add Contact Us management features including admin page, form configuration, and services management
    - Implemented Contact Us admin page with CRUD functionality.
    - Added ContactHeader, ContactInformation, ContactServices, and ContactForm components for managing contact details.
    - Introduced CreateContactUsPage for creating and editing contact information.
    - Enhanced AppSidebar to include navigation for Contact Us section.
    - Integrated debounced search and pagination for better data handling.
    - Created reusable components for form fields and services management.

--- urbanlife-frontend/src/App.jsx
+++ urbanlife-frontend/src/App.jsx
@@ -44,6 +44,13 @@ const AboutUs = lazy(() => import("./pages/AdminDashboard/AboutUs/AboutUs"));
 const CreateAboutUsPage = lazy(() =>
   import("./pages/AdminDashboard/AboutUs/CreateAboutUsPage")
 );
+
+const ContactUsAdmin = lazy(() => import("./pages/AdminDashboard/ContactUs/ContactUsAdmin"));
+const CreateContactUsPage = lazy(() =>
+  import("./pages/AdminDashboard/ContactUs/CreateContactUsPage")
+);
+
+
 const Customer = lazy(() => import("./pages/AdminDashboard/Customer/Customer"));
 const WhatsappConnect = lazy(() =>
   import("./pages/AdminDashboard/WhatsappSetting/WhatsappConnect")
@@ -179,6 +186,9 @@ function App() {
               <Route path="AboutUs" element={<AboutUs />} />
               <Route path="aboutus/create" element={<CreateAboutUsPage />} />
               <Route path="aboutus/edit/:id" element={<CreateAboutUsPage />} />
+              <Route path="contact-us" element={<ContactUsAdmin />} />
+              <Route path="contact-us/create" element={<CreateContactUsPage />} />
+              <Route path="contact-us/edit/:id" element={<CreateContactUsPage />} />
               <Route path="news" element={<News />} />
               <Route path="news/create" element={<CreateNews />} />
               <Route path="news/edit/:id" element={<CreateNews />} />

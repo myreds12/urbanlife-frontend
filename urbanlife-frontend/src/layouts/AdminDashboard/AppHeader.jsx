dd97ebba7a3ef63842010d0e758a16c33e6eb097
fheryfebrians <fheryfebrians@gmail.com>
2025-10-31 13:39:17 +0700

    change: admin dashboard name

--- urbanlife-frontend/src/layouts/AdminDashboard/AppHeader.jsx
+++ urbanlife-frontend/src/layouts/AdminDashboard/AppHeader.jsx
@@ -9,7 +9,7 @@ const AppHeader = () => {
   return (
     <header className="fixed top-0 left-64 right-0 bg-white border-b border-gray-200 z-40 p-3 px-5">
       <div className="flex items-center justify-between">
-        <h1 className="text-2xl font-bold text-gray-900">{role === "super_admin" ? "Dashboard Super Admin" : "Dashboard Admin" }</h1>
+        <h1 className="text-2xl font-bold text-gray-900">{role === "super_admin" ? "Super Admin Dashboard" : "Admin Dashboard" }</h1>
         <div className="flex items-center space-x-4">
           {/* <ThemeToggleButton /> */}
           <NotificationDropdown />
